e63709d9cae9977ece08e1a456ccb55894c4a07d
TkTJP <tktjpop@gmail.com>
2025-10-29 11:27:01 +0900

    Update db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -1,3 +1,8 @@
 <?php
+    const SERVER = 'mysql327.phy.lolipop.lan';
+    const DBNAME = 'LAA1607578-php';
+    const USER = 'LAA1607578';
+    const PASS = 'ecsite';
 
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
